b58fe2912292b1d7a4ad4f8fe5243c780ac3d12b
DragotBot1 <98428605+DragotBot1@users.noreply.github.com>
2022-01-25 16:45:25 -0600

    Update and rename src/help.js to origen/help.js

---
+++ origen/help.js
@@ -0,0 +1,117 @@
+const help = (prefix) => {
+	return `
+
+⌜ 🍒𝘿𝙧𝙖𝙜𝙤𝙣𝘽𝙤𝙩 𝙗𝙮 𝘿𝙧𝙖𝙜𝙤𝙣🍒 ⌟  
+
+◉ *INFORMACION*
+   ○ Comando: ⌜ ${prefix} ⌟
+   ○ Creador: Dragón™ 
+   ○ Mi Instagram: http://instagram.com/dragon_fuckk
+
+◉ *COMUNICADO*
+Para comunicarte con el dueño del bot, por favor poner *creador
+
+◉ *PARA USAR EL BOT*
+Registrate con el comando ${prefix}daftar y tu nombre
+
+◉ *NUEVOS COMANDOS*
+○ ${prefix}banmenu
+Banear a personas el uso del bot
+○ ${prefix}levelmenu
+Level para tus grupos
+○ ${prefix}antimenu
+Su nombre lo dice todo (antilink)
+○ ${prefix}otak
+Monas chinas
+○ ${prefix}shantera
+Interactua con el bot
+
+◉ *NUEVOS MENUS*
+○ ${prefix}juegos
+Diviértete con tus amigos :)
+○ ${prefix}desmenu
+Descargar musica y videos
+○ ${prefix}version
+Conoce la versión de tu bot
+○ ${prefix}welmenu
+Comando de bienvenida solo grupos
+
+◉ *RESUELVE TUS DUDAS*
+  ║
+  ╠🔥 ○ ${prefix}creador
+  ╚🔥 Dudas o problemas aqui
+≪━─━─━─━─━─━─◈─━─━──━─━━─━≫
+◉ *CREAR STICKERS*
+  ║
+  ╠🔥 ○ ${prefix}sticker
+  ╠🔥 ○ ${prefix}attp
+  ╠🔥 Coloca un texto pero sin emojis no tildes
+  ╠🔥 ○ ${prefix}stickergif
+  ╚🔥 6 segundos de video
+≪━─━─━─━─━─━─◈─━─━──━─━━─━≫
+◉ *CONVERTIDORES*
+  ║
+  ╠🔥 ○ ${prefix}toimg
+  ╠🔥 De sticker a JPG
+  ╠🔥 ○ ${prefix}tomp3
+  ╚🔥 De video a MP3
+≪━─━─━─━─━─━─◈─━─━──━─━━─━≫
+◉ *AUDIO*
+  ║
+  ╠🔥 ○ ${prefix}idioma
+  ╚🔥 ○ ${prefix}tts es (mas texto)
+≪━─━─━─━─━─━─◈─━─━──━─━━─━≫
+◉ *OTROS*
+  ║
+  ╠🔥 ○ ${prefix}wame
+  ╠🔥 Link de Whatsapp
+  ╠🔥 ○ ${prefix}qrcode
+  ╚🔥 Coloca un texto
+≪━─━─━─━─━─━─◈─━─━──━─━━─━≫
+◉ *GRUPOS*
+  ║
+  ╠🔥 ○ ${prefix}unir
+  ╠🔥 Unir a una persona al grupo
+  ╠🔥 ○ ${prefix}fgc
+  ╠🔥 Cambiar foto del grupo
+  ╠🔥 ○ ${prefix}ngc
+  ╠🔥 Cambiar nombre del grupo
+  ╠🔥 ○ ${prefix}dgc
+  ╠🔥 Cambiar descripcion del grupo
+  ╠🔥 ○ ${prefix}closegc
+  ╠🔥 Cerrar el grupo solo admins
+  ╠🔥 ○ ${prefix}opengc
+  ╠🔥 Abrir grupo solo admins
+  ╠🔥 ○ ${prefix}promote
+  ╠🔥 Dar admin a un miembro
+  ╠🔥 ○ ${prefix}demote
+  ╠🔥 Quitar el admin
+  ╠🔥 ○ ${prefix}kick
+  ╠🔥 Agrega a la persona que deseás eliminar
+  ╠🔥 ○ ${prefix}linkgc
+  ╠🔥 Link del grupo
+  ╠🔥 ○ ${prefix}adminlist
+  ╠🔥 Nombra a los admins del grupo
+  ╠🔥 ○ ${prefix}x
+  ╠🔥 Envia un texto para nombrar a todos
+  ╠🔥 ○ ${prefix}todos
+  ╚🔥 Nombra a todos los del grupo
+
+Para usar estas funciones el bot necesita admin
+  
+◉ *NSWF* 
+  ║
+  ╚🔥 ○ ${prefix}nsfwmenu
+
+Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
+
+
+No te olvides de seguirme en instagram Gil ;)
+
+
+𝙗𝙮 𝘿𝙧𝙖𝙜𝙤𝙣
+
+
+}
+
+exports.help = help
